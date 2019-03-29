1b39f11526cc68f3e335a0fdf9ca3476be627529
Kal1na-w <kalina.vlad2510@gmail.com>
2019-03-29 20:23:14 +0200

    Add docker and travis CI

---
+++ src/test/java/ua/od/atomspace/fractionCalculator/controller/CalculatorControllerTests.java
@@ -0,0 +1,32 @@
+package ua.od.atomspace.fractionCalculator.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ua.od.atomspace.fractionCalculator.AbstractTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorControllerTests extends AbstractTest {
+
+    @Override
+    @Before
+    public void setUp() {
+        super.setUp();
+    }
+    @Test
+    public void postEquation() throws Exception {
+        String uri = "/calc";
+        Map<String,String> jsonSend = new HashMap<>();
+        jsonSend.put("equation","2+5/5");
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON_VALUE).content(mapToJson(jsonSend))).andReturn();
+        jsonSend.put("result","3/1");
+        assertEquals(200, mvcResult.getResponse().getStatus());
+        assertEquals(jsonSend,mapFromJson(mvcResult.getResponse().getContentAsString(),HashMap.class));
+    }
+}
---
+++ src/test/java/ua/od/atomspace/fractionCalculator/controller/StatusControllerTests.java
@@ -0,0 +1,34 @@
+package ua.od.atomspace.fractionCalculator.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ua.od.atomspace.fractionCalculator.AbstractTest;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class StatusControllerTests extends AbstractTest {
+
+	@Override
+	@Before
+	public void setUp() {
+		super.setUp();
+	}
+
+	@Test
+	public void getStatus() throws Exception {
+		String uri = "/healthcheck";
+		HashMap<String,String> status = new HashMap<>();
+		status.put("status","ok");
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
+		assertEquals(200, mvcResult.getResponse().getStatus());
+		assertEquals(mapToJson(status),mvcResult.getResponse().getContentAsString());
+	}
+
+
+}
