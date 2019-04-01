4017dc39118f26fb715247033deb7ea6e8f65be3
Kal1na-w <kalina.vlad2510@gmail.com>
2019-04-01 15:32:28 +0300

    Add fraction calculator tests

--- src/main/java/ua/od/atomspace/Calculator/controller/CalculatorController.java
+++ src/main/java/ua/od/atomspace/Calculator/controller/CalculatorController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import ua.od.atomspace.Calculator.service.calculate.Calculator;
+import ua.od.atomspace.Calculator.service.FractionOperations.calculate.Calculator;
 
 import java.util.Map;
 
