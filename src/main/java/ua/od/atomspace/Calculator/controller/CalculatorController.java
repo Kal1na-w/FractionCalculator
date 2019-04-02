15a70b99114b08b52a820e1f512fbee53170d17f
Kal1na-w <kalina.vlad2510@gmail.com>
2019-04-02 19:26:56 +0300

    Add text plain

--- src/main/java/ua/od/atomspace/Calculator/controller/CalculatorController.java
+++ src/main/java/ua/od/atomspace/Calculator/controller/CalculatorController.java
@@ -1,11 +1,11 @@
 package ua.od.atomspace.Calculator.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import ua.od.atomspace.Calculator.service.FractionOperations.calculate.Calculator;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -23,5 +23,17 @@ public class CalculatorController {
         equation.put("result",calculator.calculate(equation.get("equation")));
         return equation;
     }
+
+    @RequestMapping(value = "/calculate", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String plainTextAnnotation(@RequestBody String text) {
+        String[] requestText = text.split("=");
+        StringBuilder responseText = new StringBuilder();
+        responseText.append(text);
+        responseText.append("&");
+        responseText.append("result=");
+        responseText.append(calculator.calculate(requestText[1]));
+        return responseText.toString();
+    }
+
 }
 
